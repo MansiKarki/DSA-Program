1974ada9bb3b4443ab860ce57a65178cf8145291
MansiKarki <mansikarki23@gmail.com>
2025-08-02 13:29:13 +0530

    .

---
+++ Basic Program/palindrome.java
@@ -0,0 +1,27 @@
+import java.util.Scanner;
+
+public class palindrome {
+       public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter a number: ");
+        int num = sc.nextInt();
+        int reversedNum = 0;
+          int originalNum = num;
+        while(num !=0)
+        {
+            int digit = num % 10;
+            reversedNum=reversedNum * 10 + digit;
+            num = num / 10;
+        }
+          System.out.println("Reversed number: " + reversedNum);
+ if(reversedNum ==  originalNum) 
+   {
+    System.out.println("The number is a palindrome.");
+   }
+   else{
+    System.out.println("The number is not a palindrome.");
+   }
+    sc.close();
+    }
+  
+}
