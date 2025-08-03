8cbef48f4091658fa2a7dfad7430c19a8a1bddd4
MansiKarki <mansikarki23@gmail.com>
2025-08-03 22:51:18 +0530

    .

---
+++ Pattern/rectangle.java
@@ -0,0 +1,10 @@
+public class rectangle {
+     public static void main(String[] args) {
+        for (int i = 1; i <= 3; i++) {
+            for (int j = 1; j <= 12; j++) {
+                System.out.print("*");
+            }
+            System.out.println(); // Move to next line after each row
+        }
+    }
+}
