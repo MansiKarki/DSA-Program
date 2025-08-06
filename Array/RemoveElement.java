c146957de98f254568f6140294124dbdb5615527
MansiKarki <mansikarki23@gmail.com>
2025-08-06 19:58:01 +0530

    .

---
+++ Array/RemoveElement.java
@@ -0,0 +1,29 @@
+import java.util.Arrays;
+
+public class RemoveElement {
+    public int removeElement(int[] nums, int val) {
+        int i = 0;
+        for (int j = 0; j < nums.length; j++) {
+            if (nums[j] != val) {
+                nums[i] = nums[j];
+                i++;
+            }
+        }
+        return i;
+    }
+
+    public static void main(String[] args) {
+        RemoveElement re = new RemoveElement();
+        
+        int[] nums = {3, 2, 2, 3};
+        int val = 3;
+
+        int newLength = re.removeElement(nums, val);
+
+        System.out.println("New length: " + newLength);
+        System.out.print("Updated array: ");
+        for (int i = 0; i < newLength; i++) {
+            System.out.print(nums[i] + " ");
+        }
+    }
+}
