13de42771f1eef684eacf2f5e6ead1ff6b599b00
MansiKarki <mansikarki23@gmail.com>
2025-08-06 19:52:56 +0530

    .

--- Array/SerachInsert.java
+++ Array/SerachInsert.java
@@ -9,8 +9,8 @@ class SerachInsert {
     }
     public static void main(String[] args) {
         SerachInsert obj = new SerachInsert();
-        int[] arr = {1, 3, 5, 6};
+        int[] arr = {1, 3, 5, 6,7,8,9};
         int target1 = 5;
-        System.out.println("Insert position for 5: " + obj.searchInsert(arr, target1)); // Expect: 2
+        System.out.println("Insert position for 5: " + obj.searchInsert(arr, target1));
     }
 }
