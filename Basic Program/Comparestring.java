1d5b4333a8eb55905e40275e88579732de2464ad
MansiKarki <mansikarki23@gmail.com>
2025-08-07 23:00:55 +0530

    .

---
+++ Basic Program/Comparestring.java
@@ -0,0 +1,12 @@
+public class Comparestring {
+    public static void main(String[] args) {
+        String str1 = "hello";
+        String str2 = "world";
+
+        // Compare strings using equals method
+        boolean areEqual = str1.equals(str2);
+        System.out.println("Are the strings equal? " + areEqual);
+
+       
+    }
+}
