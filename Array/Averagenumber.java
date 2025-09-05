796859a3f6255084b215744da8c2eaa4cd306a73
MansiKarki <mansikarki23@gmail.com>
2025-09-05 18:13:10 +0530

    .

--- Array/Averagenumber.java
+++ Array/Averagenumber.java
@@ -8,7 +8,7 @@ class Averagenumber{
         int sum = 0;
         
         for (int i = 0; i < n; i++)
-            sum += a[i];
+            sum = sum + a[i];
     
         return (double)sum / n;
     }
@@ -16,7 +16,7 @@ class Averagenumber{
       public static void main (String[] args)
     {
         
-        int arr[] = {10, 2, 3, 4, 5, 6, 7, 8, 9};
+        int arr[] = {10, 20,30,40,50};
         int n = arr.length;
     
         System.out.println(average(arr, n));
