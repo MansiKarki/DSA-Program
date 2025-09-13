8f51feaf67bd264ea43423e0af367a6cd9a106bb
MansiKarki <mansikarki23@gmail.com>
2025-09-13 16:02:32 +0530

    .

--- Basic Program/fact.java
+++ Basic Program/fact.java
@@ -8,7 +8,7 @@ public class fact {
         
         int fact = 1;
         for (int i = 1; i <= num; i++) {
-            fact = fact * i; // multiply fact by i
+            fact = fact * i; 
         }
         
         System.out.println("Factorial of " + num + " is " + fact);
