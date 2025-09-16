4dccc553e1faacaeadc99077a52881d687f5486d
MansiKarki <mansikarki23@gmail.com>
2025-09-17 00:11:35 +0530

    .

--- Sorting/Bubble Sort/Bubblesort.java
+++ Sorting/Bubble Sort/Bubblesort.java
@@ -2,7 +2,7 @@ import java.util.Arrays;
 
 public class Bubblesort {
     public static void main(String[] args) {
-        int[] arr= {1,2,4,5,3};
+        int[] arr= {1,2,4,5,3,5,7};
         Bubble(arr);
         System.out.println(Arrays.toString(arr));
     }
