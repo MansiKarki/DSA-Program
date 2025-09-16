8610c93d85477a225af8a440abd03db356bb1bb6
MansiKarki <mansikarki23@gmail.com>
2025-09-17 00:06:03 +0530

    .

--- Sorting/Bubble Sort/Practice.java
+++ Sorting/Bubble Sort/Practice.java
@@ -1,7 +1,7 @@
 import java.util.Arrays;
 public class Practice {
      public static void main(String[] args) {
-        int[] arr={1,3,4,2,5};
+        int[] arr={1,3,4,2,5,7,5,6};
          bubble(arr);
         System.out.println(Arrays.toString(arr));
         
