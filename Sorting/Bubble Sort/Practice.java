d5d2ac776e1f684108e1ab82e3ade08bf85d1eb5
MansiKarki <mansikarki23@gmail.com>
2025-09-16 23:13:48 +0530

    .

--- Sorting/Bubble Sort/Practice.java
+++ Sorting/Bubble Sort/Practice.java
@@ -8,10 +8,10 @@ public class Practice {
      }
      static void bubble(int[] arr){
         
-        for(int i = 0 ; i< arr.length; i++){ // it can be use to iterarte the ellemt
-            for(int j=1; j<=arr.length-1;j++){
-                if(arr[j]<arr[j-1]){
-                    int temp = arr[j];
+        for(int i = 0 ; i< arr.length; i++){ // it can be use to iterarte the element
+            for(int j=1; j<=arr.length-1;j++){// the number interal number will be reduce after each iteration
+                if(arr[j]<arr[j-1]){ 
+                    int temp = arr[j];//if the item is  smaller than previous item then swap
                     arr[j] = arr[j-1];
                     arr[j-1] = temp;
                 }
