dd9fbe0f67b41d1fd5f8ec7503994b6d4910e805
MansiKarki <mansikarki23@gmail.com>
2025-09-20 23:55:49 +0530

    .

---
+++ Basic Program/SimpleClassMetho.java
@@ -0,0 +1,23 @@
+import java.util.*;
+public class SimpleClassMetho {
+    String name;
+    String addd;
+    void accept(){
+  System.out.println("Enter the Name of the Student:");
+    Scanner sc = new Scanner(System.in);
+    name = sc.nextLine();
+    System.out.println("Enter the Address:");
+    addd = sc.nextLine();
+    }
+    void display(){
+        System.out.println("Student Name and Address are displayed");
+        System.out.println("Name: "+name);
+        System.out.println("Address: "+addd);
+
+    }
+    public static void main(String[] args) {
+        SimpleClassMetho obj = new SimpleClassMetho();
+        obj.accept();
+        obj.display();
+    }
+}
