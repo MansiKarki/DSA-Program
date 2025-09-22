d523020fb9ff3b266835e5185f88e7fb0ca891eb
MansiKarki <mansikarki23@gmail.com>
2025-09-23 01:17:00 +0530

    .

--- Basic Program/Employee.java
+++ Basic Program/Employee.java
@@ -4,17 +4,17 @@ public class Employee {
     String name , Place ;
     int salary;
     void Read(){
-        System.out.println("Enter the Name Of Employee");
+        System.out.println("Enter the Name Of emp");
         name = sc.next();
-        System.out.println("Enter the Place Of Employee");
+        System.out.println("Enter the Place Of emp");
         Place = sc.next();
-        System.out.println("Enter the salary Of Employee");
+        System.out.println("Enter the salary Of emp");
         salary = sc.nextInt();
         
 
     }
     void display(){
-        System.out.println("The Name Of Employee :  " + name);
+        System.out.println("The Name Of emp :  " + name);
         System.out.println("Current Place : " + Place);
         System.out.println("Per Month Salary :" + salary);
     }
