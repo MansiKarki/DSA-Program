2ce128f40bb752bf402472e2332adc0bd3d78977
MansiKarki <mansikarki23@gmail.com>
2025-09-23 01:16:01 +0530

    .

---
+++ Basic Program/RepresentString.java
@@ -0,0 +1,8 @@
+public class RepresentString {
+    public static void main(String[] args) {
+        String str = "Hello, World!";
+        System.out.println(str);
+        String s1 = new String("Hello, Java!");
+        System.out.println(s1);
+    }
+}
