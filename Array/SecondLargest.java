dbf392ce3038bfb16ba7a81aac745460c2078669
MansiKarki <mansikarki23@gmail.com>
2025-10-03 19:32:10 +0530

    .

---
+++ Array/SecondLargest.java
@@ -0,0 +1,27 @@
+import java.util.Arrays;
+
+public class SecondLargest {
+    public static void main(String[] args) {
+        int[] arr = {12, 35, 1, 10, 34, 1};
+
+        Arrays.sort(arr); // sort array in ascending order
+
+        int n = arr.length;
+        int largest = arr[n - 1];
+        int secondLargest = -1;
+
+        // find element just smaller than largest
+        for (int i = n - 2; i >= 0; i--) {
+            if (arr[i] != largest) {
+                secondLargest = arr[i];
+                break;
+            }
+        }
+
+        if (secondLargest == -1) {
+            System.out.println("No second largest element found.");
+        } else {
+            System.out.println("Second Largest Element is: " + secondLargest);
+        }
+    }
+}
