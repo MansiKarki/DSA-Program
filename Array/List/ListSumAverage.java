30ab8798ae9bfc79f44a2fd883971b0b0e353b9c
MansiKarki <mansikarki23@gmail.com>
2025-10-07 23:47:11 +0530

    .

---
+++ Array/List/ListSumAverage.java
@@ -0,0 +1,18 @@
+import java.util.*;
+
+public class ListSumAverage {
+    public static void main(String[] args) {
+        List<Integer> numbers = Arrays.asList(10, 20, 30, 40, 50);
+
+        int sum = 0;
+        for (int num : numbers) {
+            sum += num;
+        }
+
+        double average = (double) sum / numbers.size();
+
+        System.out.println("Numbers: " + numbers);
+        System.out.println("Sum: " + sum);
+        System.out.println("Average: " + average);
+    }
+}
