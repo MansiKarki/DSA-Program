2bc194ad1c9dde5f3dffe4d55777c7c2385341e3
MansiKarki <mansikarki23@gmail.com>
2025-10-07 23:52:50 +0530

    .

---
+++ Array/List/SimpleList.java
@@ -0,0 +1,13 @@
+import java.util.*;
+
+public class SimpleList {
+    public static void main(String[] args) {
+        List<String> names = new ArrayList<>();
+
+        names.add("Mansi");
+        names.add("Riya");
+        names.add("Aman");
+
+        System.out.println("Names in the list: " + names);
+    }
+}
