2ff228d262af4f0d544a1b5c9dca2d1dc361c7bd
MansiKarki <mansikarki23@gmail.com>
2025-10-11 22:40:36 +0530

    .

--- Array/List/evenorodd.java
+++ Array/List/evenorodd.java
@@ -2,7 +2,7 @@ import java.util.*;
 
 public class evenorodd {
     public static void main(String[] args) {
-        List<Integer> nums = Arrays.asList(1,2,3,4,5,6,7,8,9,10);
+        List<Integer> nums = Arrays.asList(1,2,3,4,5,6,7,8,9,10,11,12,13,15);
 
         System.out.println("Numbers: " + nums);
 
