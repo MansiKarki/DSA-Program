5065a6e1c0b61e1d70e563ad97cc19dec324bf8f
MansiKarki <232305284+MansiKarki23@users.noreply.github.com>
2025-10-13 14:21:11 +0530

    .

---
+++ Array/List/palindrome usinf stringbuilder.java
@@ -0,0 +1,20 @@
+import java.util.*;
+
+public class palindromeusingstringbuilder {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter a word: ");
+        String word = sc.nextLine();
+
+        String reversed = new StringBuilder(word).reverse().toString();
+
+        if (word.equalsIgnoreCase(reversed)) {
+            System.out.println(word + " is a palindrome.");
+        } else {
+            System.out.println(word + " is not a palindrome.");
+        }
+
+        sc.close();
+    }
+}
+
