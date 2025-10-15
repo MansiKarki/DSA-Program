6ddffff3e918fce4b47bf65a42c0a3e82d4f0531
MansiKarki <mansikarki23@gmail.com>
2025-10-15 22:24:24 +0530

    .

--- JS/script.js
+++ JS/script.js
@@ -131,7 +131,7 @@ const classinfo={
         grade:"C"
     },
 };
-let fruit = ["apple", "banana", "mango", "grapes"];
+let fruit = ["apple", "banana", "mango", "grapes","CHIKOO"];
 for(let i=0; i<fruit.length; i++)
 {
     console.log(fruit[i]);
