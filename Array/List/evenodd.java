dda1df43b8b6553cdd90ddd563ee53ba3f32bb36
MansiKarki <mansikarki23@gmail.com>
2025-10-31 00:50:03 +0530

    ..

---
+++ Array/List/evenodd.java
@@ -0,0 +1,19 @@
+import java.util.*;
+
+public class evenorodd {
+    public static void main(String[] args) {
+        List<Integer> nums = Arrays.asList(1,2,3,4,5,6,7,8,9,10,11,12,13,15);
+
+        System.out.println("Numbers: " + nums);
+
+        System.out.print("Even: ");
+        for (int n : nums)
+            if (n % 2 == 0)
+                System.out.print(n + " ");
+
+        System.out.print("\nOdd: ");
+        for (int n : nums)
+            if (n % 2 != 0)
+                System.out.print(n + " ");
+    }
+}
